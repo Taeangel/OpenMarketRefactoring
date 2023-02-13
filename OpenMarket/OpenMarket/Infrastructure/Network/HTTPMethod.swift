94d47621b8704c6af6ad069170a3c5f0611616a0
Taeangel <wngks2050@gmail.com>
2023-02-13 18:43:11 +0900

    feat: HTTPMethod 생성

---
+++ OpenMarket/OpenMarket/Infrastructure/Network/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  OpenMarket
+//
+//  Created by song on 2023/02/13.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+  case get = "GET"
+  case post = "POST"
+  case delete = "DELETE"
+  case patch = "PATCH"
+}
