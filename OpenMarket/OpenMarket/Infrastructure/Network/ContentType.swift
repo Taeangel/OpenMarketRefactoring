67032ed4c82a0f0b78d0f8874213e2108d0026b7
Taeangel <wngks2050@gmail.com>
2023-02-13 18:42:51 +0900

    feat: ContentType 생성

---
+++ OpenMarket/OpenMarket/Infrastructure/Network/ContentType.swift
@@ -0,0 +1,13 @@
+//
+//  ContentType.swift
+//  OpenMarket
+//
+//  Created by song on 2023/02/13.
+//
+
+import Foundation
+
+enum ContentType: String {
+  case json = "application/json"
+  case jpeg = "image/jpeg"
+}
