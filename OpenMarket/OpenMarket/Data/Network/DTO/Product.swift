cb95a434c251ccc4cf275e38412eddc3150fe01d
Taeangel <wngks2050@gmail.com>
2023-02-13 18:40:02 +0900

    feat: ProductRequestDTO 생성

---
+++ OpenMarket/OpenMarket/Data/Network/DTO/Product.swift
@@ -0,0 +1,29 @@
+//
+//  Product.swift
+//  OpenMarket
+//
+//  Created by song on 2023/02/13.
+//
+
+import Foundation
+
+struct ProductRequestDTO: Encodable {
+  let name: String
+  let description: String
+  let price: Int
+  let currency: String
+  let discountedPrice: Int
+  let stock: Int
+  let secret: String = "bjv33pu73cbajp1"
+  let boundary: String = UUID().uuidString
+  
+  enum CodingKeys: String, CodingKey {
+    case name
+    case description
+    case price
+    case currency
+    case discountedPrice = "discounted_price"
+    case stock
+    case secret
+  }
+}
