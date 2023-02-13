424a3f96c262d38a4721a176348a9bc1c234b8c3
Taeangel <wngks2050@gmail.com>
2023-02-13 18:40:29 +0900

    feat: Currency 생성

---
+++ OpenMarket/OpenMarket/Domain/Entities/Currency.swift
@@ -0,0 +1,13 @@
+//
+//  Currency.swift
+//  OpenMarket
+//
+//  Created by song on 2023/02/13.
+//
+
+import Foundation
+
+enum Currency: String, CaseIterable, Codable {
+  case KRW = "KRW"
+  case USD = "USD"
+}
