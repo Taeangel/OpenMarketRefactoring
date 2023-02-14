bced455b58dc3a96403cfdc2643b5c22004715b7
Taeangel <wngks2050@gmail.com>
2023-02-14 11:44:48 +0900

    feat: AppDIContainer 생성

---
+++ OpenMarket/OpenMarket/DIContainer/DIContainer.swift
@@ -0,0 +1,21 @@
+//
+//  DIContainer.swift
+//  OpenMarket
+//
+//  Created by song on 2023/02/14.
+//
+
+import Foundation
+
+protocol AppDIContainerable {
+  
+}
+
+final class AppDIContainer: AppDIContainerable {
+  private let apiManager = ApiManager(session: URLSession.shared)
+  private let openMarketStorage: OpenMarketStorage
+
+  init() {
+    self.openMarketStorage = .init(openMarketApiManager: apiManager)
+  }
+}
--- OpenMarket/OpenMarket/Data/Storages/OpenMarketStorage.swift
+++ OpenMarket/OpenMarket/Data/Storages/OpenMarketStorage.swift
@@ -24,15 +24,11 @@ final class OpenMarketStorage {
 extension OpenMarketStorage: OpenMarketStorageable {
   func fetchProductList() -> RxSwift.Observable<PoductListDTO> {
     return openMarketApiManager.requestObservable(.getProductList())
-      .compactMap {
-        return try? JSONDecoder().decode(PoductListDTO.self, from: $0)
-      }
+      .compactMap { try? JSONDecoder().decode(PoductListDTO.self, from: $0) }
   }
   
   func fetchProduct(_ id: Int) -> RxSwift.Observable<ProductDTO> {
     return openMarketApiManager.requestObservable(.getProduct(id))
-      .compactMap {
-        return try? JSONDecoder().decode(ProductDTO.self, from: $0)
-      }
+      .compactMap { try? JSONDecoder().decode(ProductDTO.self, from: $0) }
   }
 }
