65cbfd946d8a049b5a86118958bf5f16e78a3fa8
Taeangel <wngks2050@gmail.com>
2023-02-28 03:52:03 +0900

    refactor: ProductListViewModel에 action함수 생성

--- OpenMarket/OpenMarket/Presenter/ProductListView/ViewModel/ProductListViewModel.swift
+++ OpenMarket/OpenMarket/Presenter/ProductListView/ViewModel/ProductListViewModel.swift
@@ -10,11 +10,18 @@ import RxSwift
 import RxRelay
 
 protocol ProductListViewModelable {
-  func updateList()
   var productListObservable: BehaviorRelay<[BasicProductEntity]> { get set }
+  var delegate: ProductListViewModelDelegate? { get set }
+  func updateList()
+  func action(action: ProductListViewModel.ViewAction)
+}
+
+protocol ProductListViewModelDelegate: AnyObject {
+  func coordinatorShowProductView(productID: Int)
 }
 
 final class ProductListViewModel: ProductListViewModelable {
+  weak var delegate: ProductListViewModelDelegate?
   private let disposeBag: DisposeBag
   private let fetchUseCase: FetchUseCaseable
   var productListObservable: BehaviorRelay<[BasicProductEntity]>
@@ -27,11 +34,29 @@ final class ProductListViewModel: ProductListViewModelable {
     updateList()
   }
   
+  func action(action: ViewAction) {
+    switch action {
+    case let .productTap(productID):
+      delegate?.coordinatorShowProductView(productID: productID)
+    case .updataList:
+      updateList()
+    }
+  }
+  
   func updateList() {
     fetchUseCase.fetchProductList(pageNum: 1)
       .compactMap { $0.product }
       .map { $0.map { $0.toEneity()} }
       .bind(to: productListObservable)
       .disposed(by: disposeBag)
+  }
+}
+
+// MARK: - Action
+
+extension ProductListViewModel {
+  enum ViewAction {
+    case productTap(Int)
+    case updataList
   }
 }
