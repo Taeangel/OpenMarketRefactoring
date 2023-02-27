4ecee9cebecba7bcd830a347dc138843d68030b7
Taeangel <wngks2050@gmail.com>
2023-02-28 02:29:52 +0900

    folder: VendorsEntity 파일추가

---
+++ OpenMarket/OpenMarket/Domain/Entities/VendorEntity.swift
@@ -0,0 +1,13 @@
+//
+//  VendorEntity.swift
+//  OpenMarket
+//
+//  Created by song on 2023/02/28.
+//
+
+import Foundation
+
+struct VendorsEntity: Codable {
+  let id: Int?
+  let name: String?
+}
